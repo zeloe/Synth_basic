15255f5af0192723820ed8aabb8255dd5ea353be
Zeno Lösch <54742442+zeloe@users.noreply.github.com>
2023-05-28 10:39:52 +0200

    basic wavetables

---
+++ Source/WaveTables/Sawtooth.h
@@ -0,0 +1,38 @@
+/*
+  ==============================================================================
+
+    Sawtooth.h
+    Created: 22 Apr 2023 3:03:00pm
+    Author:  Onez
+
+  ==============================================================================
+*/
+
+#pragma once
+#include "JuceHeader.h"
+class Sawtooth
+{
+public:
+    Sawtooth()
+    {
+        // wird nur einmal in Constructor ausgeführt
+        createWavetable();
+    };
+    ~Sawtooth(){};
+    
+    void createWavetable()
+    {
+        //code für wavetable
+        waveTable.setSize (1, (int) tableSize + 1);
+        waveTable.clear();
+        auto* samples = waveTable.getWritePointer (0);
+        for(int i = 0; i < tableSize; ++i)
+        {
+            samples[i] = (float(i) / float (tableSize));
+        }
+    }
+    
+    juce::AudioSampleBuffer waveTable;
+private:
+    const unsigned int tableSize = 512;
+};
